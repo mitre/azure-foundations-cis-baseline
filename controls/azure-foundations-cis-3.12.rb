fd41d65a24beece99221325b63efe421d0c17963
seanlongcc <seanlongcc@gmail.com>
2025-01-16 13:15:28 -0500

    up to 3.17

--- controls/azure-foundations-cis-3.12.rb
+++ controls/azure-foundations-cis-3.12.rb
@@ -1,22 +1,66 @@
-control 'azure-foundations-cis-1.1.1' do
-    title "title"
-    desc "description"
+control 'azure-foundations-cis-3.12' do
+    title "Ensure Storage for Critical Data are Encrypted with Customer Managed Keys (CMK)"
+    desc "Enable sensitive data encryption at rest using Customer Managed Keys (CMK) rather
+        than Microsoft Managed keys."
 
     desc 'rationale',
-        "description"
+        "By default, data in the storage account is encrypted using Microsoft Managed Keys at
+        rest. All Azure Storage resources are encrypted, including blobs, disks, files, queues,
+        and tables. All object metadata is also encrypted. If you want to control and manage this
+        encryption key yourself, however, you can specify a customer-managed key. That key is
+        used to protect and control access to the key that encrypts your data. You can also
+        choose to automatically update the key version used for Azure Storage encryption
+        whenever a new version is available in the associated Key Vault."
+
+    desc 'impact',
+        "If the key expires by setting the 'activation date' and 'expiration date', the user must
+        rotate the key manually.
+        Using Customer Managed Keys may also incur additional man-hour requirements to
+        create, store, manage, and protect the keys as needed."
 
     desc 'check',
-       "description"
+       "From Azure Console:
+        1. Go to Storage Accounts
+        2. For each storage account, go to Encryption
+        3. Ensure that Encryption type is set to Customer Managed Keys
+        From PowerShell
+        Connect-AzAccount
+        Set-AzContext -Subscription <subscription id>
+        Get-AzStorageAccount |Select-Object -ExpandProperty Encryption
+        PowerShell Results - Non-Compliant
+        ...
+        KeySource : Microsoft.Storage
+        ...
+        Page 217
+        PowerShell Results - Compliant
+        ...
+        KeySource : Microsoft.Keyvault
+        ...
+        From Azure Policy
+        If referencing a digital copy of this Benchmark, clicking a Policy ID will open a link to the
+        associated Policy definition in Azure.
+        If referencing a printed copy, you can search Policy IDs from this URL:
+        https://portal.azure.com/#view/Microsoft_Azure_Policy/PolicyMenuBlade/~/Definitions
+        • Policy ID: 6fac406b-40ca-413b-bf8e-0bf964659c25 - Name: 'Storage accounts
+        should use customer-managed key for encryption'"
 
     desc 'fix',
-       "description"
-
-    
+       "From Azure Portal
+        1. Go to Storage Accounts
+        2. For each storage account, go to Encryption
+        3. Set Customer Managed Keys
+        4. Select the Encryption key and enter the appropriate setting value
+        5. Click Save"
 
     impact 0.5
-    tag nist: ['tag']
+    tag nist: ['IA-5(1)','SC-28','SC-28(1)']
     tag severity: 'medium'
-    tag cis_controls: [{ '8' => ['control'] }]
+    tag cis_controls: [{ '8' => ['3.11'] }]
+
+    ref 'https://docs.microsoft.com/en-us/azure/storage/common/storage-service-encryption'
+    ref 'https://docs.microsoft.com/en-us/azure/security/fundamentals/data-encryption-best-practices#protect-data-at-rest'
+    ref 'https://docs.microsoft.com/en-us/azure/storage/common/storage-service-encryption#azure-storage-encryption-versus-disk-encryption'
+    ref 'https://learn.microsoft.com/en-us/security/benchmark/azure/mcsb-data-protection#dp-5-use-customer-managed-key-option-in-data-at-rest-encryption-when-required'
 
     describe 'benchmark' do
         skip 'configure'
--- controls/azure-foundations-cis-3.14.rb
+++ controls/azure-foundations-cis-3.14.rb
@@ -1,22 +1,72 @@
-control 'azure-foundations-cis-1.1.1' do
-    title "title"
-    desc "description"
+control 'azure-foundations-cis-3.14' do
+    title "Ensure Storage Logging is Enabled for Table Service for 'Read', 'Write', and 'Delete' Requests"
+    desc "Azure Table storage is a service that stores structured NoSQL data in the cloud,
+        providing a key/attribute store with a schema-less design. Storage Logging happens
+        server-side and allows details for both successful and failed requests to be recorded in
+        the storage account. These logs allow users to see the details of read, write, and delete
+        operations against the tables. Storage Logging log entries contain the following
+        information about individual requests: timing information such as start time, end-to-end
+        latency, and server latency; authentication details; concurrency information; and the
+        sizes of the request and response messages."
 
     desc 'rationale',
-        "description"
+        "Storage Analytics logs contain detailed information about successful and failed requests
+        to a storage service. This information can be used to monitor each individual request to
+        a storage service for increased security or diagnostics. Requests are logged on a best-
+        effort basis.
+        Storage Analytics logging is not enabled by default for your storage account."
+
+    desc 'impact',
+        "Being a level 2, enabling this setting can have a high impact on the cost of data storage
+        used for logging more data per each request. Do not enable this without determining
+        your need for this level of logging or forget to check in on data usage and projected
+        cost."
 
     desc 'check',
-       "description"
+       "From Azure Portal
+        1. From the default portal page select Storage Accounts.
+        2. Select the specific Storage Account.
+        3. Click the Diagnostics settings under the Monitoring section in the left column.
+        4. Select the 'table' tab indented below the storage account. Then select the
+        diagnostic setting listed.
+        5. Ensure StorageRead, StorageWrite, and StorageDelete options are selected
+        under the Logging section and that they are sent to the correct destination.
+        Page 223
+        From Azure CLI
+        Ensure the below command's output contains properties delete, read and write set to
+        true.
+        az storage logging show --services t --account-name <storageAccountName>
+        From Azure Policy
+        If referencing a digital copy of this Benchmark, clicking a Policy ID will open a link to the
+        associated Policy definition in Azure.
+        If referencing a printed copy, you can search Policy IDs from this URL:
+        https://portal.azure.com/#view/Microsoft_Azure_Policy/PolicyMenuBlade/~/Definitions
+        • Policy ID: 2fb86bf3-d221-43d1-96d1-2434af34eaa0 - Name: 'Configure
+        diagnostic settings for Table Services to Log Analytics workspace'"
 
     desc 'fix',
-       "description"
-
-    
+       "From Azure Portal
+        1. From the default portal page select Storage Accounts.
+        2. Select the specific Storage Account.
+        3. Click the Diagnostics settings under the Monitoring section in the left column.
+        4. Select the 'table' tab indented below the storage account.
+        5. Click '+ Add diagnostic setting'.
+        6. Select StorageRead, StorageWrite and StorageDelete options under the Logging
+        section to enable Storage Logging for Table service.
+        7. Select a destination for your logs to be sent to.
+        From Azure CLI
+        Use the below command to enable the Storage Logging for Table service.
+        az storage logging update --account-name <storageAccountName> --account-key
+        <storageAccountKey> --services t --log rwd --retention 90"    
 
     impact 0.5
-    tag nist: ['tag']
+    tag nist: ['AU-3','AU-3(1)','AU-7','AU-12']
     tag severity: 'medium'
-    tag cis_controls: [{ '8' => ['control'] }]
+    tag cis_controls: [{ '8' => ['8.5'] }]
+
+    ref 'https://docs.microsoft.com/en-us/rest/api/storageservices/about-storage-analytics-logging'
+    ref 'https://docs.microsoft.com/en-us/cli/azure/storage/logging?view=azure-cli-latest'
+    ref 'https://learn.microsoft.com/en-us/security/benchmark/azure/mcsb-logging-threat-detection#lt-3-enable-logging-for-security-investigation'
 
     describe 'benchmark' do
         skip 'configure'
